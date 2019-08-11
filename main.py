7163c965f6576046885c57b208eb8e2b30f170a5
aryamanarora <aryaman.arora2020@gmail.com>
2019-08-11 17:30:43 -0400

    minor clean up

--- main.py
+++ main.py
@@ -1,41 +1,35 @@
-# needed data: orthographic forms and phonetic forms
-# convert to IPA or use existing system
-#
-# 1. align Hindi orthographic with phonetic representation
-
 import sys
 import numpy as np
 import pandas as pd
 import transliterate as tr
 import scrape
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, recall_score, f1_score
 
 UNK_CHAR = "🆒"
 
-# read in the data
 def main(input_filename, left=4, right=4, sep=None):
     data = pd.read_csv(input_filename, header=0, sep=sep) if sep else pd.read_csv(input_filename, header=0)
 
-    instances = []
+    # force align the predicted orthographic transliteration (without schwa dropping)
+    # with the actual phonetic transliteration (schwa dropping) to created training/test data
+    schwa_instances = []
     for _, row in data.iterrows():
-        if _ % 1000 == 0:
-            print('Processing row', _)
+        print(chr(27) + "[2J")
+        print('Processing row', _)
         try:
-            instances += [[tr.narrow_categorize(row.hindi), schwa_instance[1], schwa_instance[0]]
+            schwa_instances += [[tr.narrow_categorize(row.hindi), schwa_instance[1], schwa_instance[0]]
                     for schwa_instance in tr.force_align(tr.transliterate(row.hindi), str(row.phon))]
         except Exception as e:
             # print(e)
             continue
     
-    # print(len(instances))
-    # input()
-
+    # clean up the data
     y = []
     transformed_instances = []
-    for s, schwa_index, schwa_was_deleted in instances:
+    for s, schwa_index, schwa_was_deleted in schwa_instances:
         x = []
         for i in range(schwa_index - left, schwa_index + right + 1):
             if i == schwa_index:
@@ -56,14 +50,15 @@ def main(input_filename, left=4, right=4, sep=None):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.20, random_state=42)
 
-    model = LogisticRegression(solver='liblinear')
+    model = LogisticRegression(solver='liblinear', max_iter=1000)
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
     # for i in zip(transformed_instances, y_pred, y_test):
     #     print(i)
 
-    return [accuracy_score(y_pred, y_test), recall_score(y_pred, y_test), f1_score(y_pred, y_test)]
+    print("Logistic regression:", [accuracy_score(y_pred, y_test), recall_score(y_pred, y_test), f1_score(y_pred, y_test)])
 
+# compare wiktionary transliterations with actual
 def compare_wiktionary():
     data = pd.read_csv('hi_ur_pron.tsv', header=0, sep='\t')
     wikt = pd.read_csv('hi_ur_pron_wikt.tsv', header=0, sep='\t')
@@ -76,5 +71,5 @@ def compare_wiktionary():
             print(w, x)
 
 if __name__ == "__main__":
-    print(main('hi_pron.csv', 4, 4))
+    main('hi_ur_pron.tsv', 4, 4, '\t')
     # compare_wiktionary()
